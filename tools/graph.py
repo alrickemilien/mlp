c911a79dd1839a5bf1411e4cb1eeaea3d7956f3f
alrickemilien <aemilien@student.42.fr>
2019-09-20 17:13:43 +0200

    Need to imptove graph by indexing and add pdf feature

--- tools/graph.py
+++ tools/graph.py
@@ -3,10 +3,62 @@
 
 import sys
 import csv
+import itertools
 from os import path
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+plot_index = 0
+
+def scatterplot_matrix(data, names, **kwargs):
+    """Plots a scatterplot matrix of subplots.  Each row of "data" is plotted
+    against other rows, resulting in a nrows by nrows grid of subplots with the
+    diagonal subplots labeled with "names".  Additional keyword arguments are
+    passed on to matplotlib's "plot" command. Returns the matplotlib figure
+    object containg the subplot grid."""
+
+    numvars, numdata = data.shape
+    fig, axes = plt.subplots(nrows=numvars, ncols=numvars, figsize=(8,8))
+    fig.subplots_adjust(hspace=0.05, wspace=0.05)
+
+    for ax in axes.flat:
+        # Hide all ticks and labels
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+
+        # Set up ticks only on one side for the "edge" subplots...
+        if ax.is_first_col():
+            ax.yaxis.set_ticks_position('left')
+        if ax.is_last_col():
+            ax.yaxis.set_ticks_position('right')
+        if ax.is_first_row():
+            ax.xaxis.set_ticks_position('top')
+        if ax.is_last_row():
+            ax.xaxis.set_ticks_position('bottom')
+
+    print('0')
+
+    # Plot the data.
+    for i, j in zip(*np.triu_indices_from(axes, k=1)):
+        for x, y in [(i,j), (j,i)]:
+            axes[x,y].plot(data[x], data[y], **kwargs)
+
+    print('A')
+
+    # Label the diagonal subplots...
+    for i, label in enumerate(names):
+        axes[i,i].annotate(label, (0.5, 0.5), xycoords='axes fraction',
+                ha='center', va='center')
+
+    print('B')
+
+    # Turn on the proper x or y axes ticks.
+    for i, j in zip(range(numvars), itertools.cycle((-1, 0))):
+        axes[j,i].xaxis.set_visible(True)
+        axes[i,j].yaxis.set_visible(True)
+
+    return fig
 
 def csv2data(dataset_path):
     with open(dataset_path, 'r') as f:
@@ -16,8 +68,8 @@ def csv2data(dataset_path):
 
 def preprocessing(data):
     data = np.array(data)
+    
     def f(v):
-        print('v', v)
         try:
             return v.astype(np.float)
         except:
@@ -25,10 +77,13 @@ def preprocessing(data):
             print('classification', classification )
 
             def indexof(vv):
-                return np.where(classification == vv)
-            return np.vstack(indexof(x) for x in v)
+                return np.where(classification == vv)[0][0]
+            return [indexof(x) for x in v]
         return data
-    return np.vstack([f(x) for x in data.T]).T
+    
+    ret = np.vstack([f(x) for x in data.T])
+    
+    return ret.T
 
 if __name__ == '__main__':
     # Extract dataset path - raise on invalid path
@@ -42,4 +97,5 @@ if __name__ == '__main__':
 
     print('data', data)
 
-    plt.scatter(data, data, c = 'red')
+    scatterplot_matrix(np.delete(data.T, np.arange(20), axis=0), [], c = 'red')
+    plt.show()
